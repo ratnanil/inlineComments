18e6b33dd88e52f1ad73b55471e9acf5eafe1cf5
Nils Ratnaweera <rata@zhaw.ch>
2021-05-10 23:25:32 +0200

    rm after clena

--- R/inlinecomment.R
+++ R/inlinecomment.R
@@ -4,23 +4,29 @@
 #'
 #' @export
 inlinecomment <- function(){
-  # cont <- rstudioapi::getActiveDocumentContext()
-  # filepath <- cont$path
-  # rootpath <- here::here()
-  # filepath <- stringr::str_remove(filepath, rootpath)
-  # firstline <- cont$selection[[1]]$range$start[[1]]
-  # lastline <- cont$selection[[1]]$range$end[[1]]
-  # remote_url <- git2r::remote_url()
-  # remote_url <- gh_username_repo(remote_url)
-  # sha <- git2r::commits(n = 1)[[1]]$sha
+  cont <- rstudioapi::getActiveDocumentContext()
+  filepath <- cont$path
+  rootpath <- here::here()
+  filepath <- stringr::str_remove(filepath, rootpath)
+  firstline <- cont$selection[[1]]$range$start[[1]]
+  lastline <- cont$selection[[1]]$range$end[[1]]
+  remote_url <- git2r::remote_url()
+  remote_url <- gh_username_repo(remote_url)
+  sha <- git2r::commits(n = 1)[[1]]$sha
 
-  # return(glue::glue("{remote_url}/blob/{sha}{filepath}#L{firstline}-L{lastline}"))
+  lineref <- glue::glue("{remote_url}/blob/{sha}{filepath}#L{firstline}-L{lastline}")
   ui <- shiny::fluidPage(
+    shiny::actionButton("done", "Create issue"),
     shiny::textInput("title", "Issue Title", width = "100%"),
-    shiny::textAreaInput("body", "Issue Text",  width = "100%"),
+    shinyAce::aceEditor("body", mode = "markdown", height = "200px"),
+
   )
   server <- function(input, output) {
-    output$value <- shiny::renderText({input$title})
+    observeEvent(input$done,{
+      res <- system(glue::glue('gh issue create --title "{input$title}" --body "{lineref}<br/><br/>{input$body}"'))
+      print(res)
+      shiny::stopApp()
+    })
   }
   shiny::shinyApp(ui, server)
 }
